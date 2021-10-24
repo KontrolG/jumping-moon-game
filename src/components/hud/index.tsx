9904c2412acbbc4722be745fe410a8870f879784
German Solano <german@famosos.com>
2021-10-24 12:05:03 -0400

    feat(<HUD />): :sparkles: Add elements and styles

---
+++ src/components/hud/index.tsx
@@ -0,0 +1,52 @@
+import "./styles.css";
+
+interface HUDProps {
+  gameStatus: string;
+  points: number;
+  onRestartGame: () => void;
+}
+
+function HUD({ gameStatus, points, onRestartGame }: HUDProps) {
+  if (gameStatus === "lost") {
+    return (
+      <div className="HUDScreen GameOverHUD">
+        <div className="GameOverHUDContent">
+          <h1>Game over</h1>
+          <button type="button" onClick={onRestartGame}>
+            Try again
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  if (gameStatus === "won") {
+    return (
+      <div className="HUDScreen GameOverHUD">
+        <div className="GameOverHUDContent">
+          <h1>You win!</h1>
+          <button type="button" onClick={onRestartGame}>
+            Play again
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="HUDScreen CounterHUD">
+      <div key={points}>{points}</div>
+      <section className="Instructions">
+        <h2>Instructions</h2>
+        <p>
+          Use the <kbd className="Key">W</kbd>, <kbd className="Key">A</kbd>,{" "}
+          <kbd className="Key">S</kbd>, <kbd className="Key">D</kbd> and{" "}
+          <kbd className="Key">SPACE</kbd> keys to move.
+        </p>
+        <p>Bounce in every orange tile to win.</p>
+      </section>
+    </div>
+  );
+}
+
+export { HUD };
