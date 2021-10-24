247cc5b61f5260b647d94bd0f47b5d202dd122de
German Solano <german@famosos.com>
2021-10-24 12:07:46 -0400

    feat(<Box />): :sparkles: Add logic to react when collided for first time

--- src/components/box/index.tsx
+++ src/components/box/index.tsx
@@ -1,21 +1,33 @@
-import { Mesh } from "three";
-import { useRef } from "react";
-import { BoxProps, useBox } from "@react-three/cannon";
+import { useRef, useState } from "react";
+import { BoxProps as BaseBoxProps, useBox } from "@react-three/cannon";
 
-function Box(props: BoxProps) {
+interface BoxProps extends BaseBoxProps {
+  onCollideForFirstTime?: () => void;
+}
+
+function Box({ onCollideForFirstTime, ...props }: BoxProps) {
+  const [hasCollided, setHasCollided] = useState(false);
+  // Using ref because onCollide function is memoized after creation.
+  const hasCollidedRef = useRef(hasCollided);
+  hasCollidedRef.current = hasCollided;
+
   const [ref] = useBox(() => ({
     type: "Static",
     mass: 10,
     rotation: [-Math.PI / 2, 0, 0],
     args: [10, 10, 0.5],
+    onCollide() {
+      if (hasCollidedRef.current) return;
+      setHasCollided(true);
+      onCollideForFirstTime?.();
+    },
     ...props
   }));
 
   return (
     <mesh ref={ref} scale={1}>
-      <arrowHelper />
       <boxGeometry args={[10, 10, 0.5]} />
-      <meshStandardMaterial color="orange" />
+      <meshStandardMaterial color={hasCollided ? "#aaa" : "orange"} />
     </mesh>
   );
 }
