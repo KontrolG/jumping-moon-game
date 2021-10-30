e3f1478140e46b0a6dfcb03962c9d40d922149aa
German Solano <kontrolg1239@gmail.com>
2021-10-30 12:20:59 -0400

    feat(<HUD />): :lipstick: Add fullscreen button and instructions for mobiel

--- src/components/hud/index.tsx
+++ src/components/hud/index.tsx
@@ -1,4 +1,5 @@
 import "./styles.css";
+import fscreen from "fscreen";
 
 interface HUDProps {
   gameStatus: string;
@@ -35,14 +36,27 @@ function HUD({ gameStatus, points, onRestartGame }: HUDProps) {
 
   return (
     <div className="HUDScreen CounterHUD">
-      <div key={points}>{points}</div>
+      <div className="CounterNumber" key={points}>
+        {points}
+      </div>
       <section className="Instructions">
-        <h2>Instructions</h2>
-        <p>
+        <h2>
+          Instructions{" "}
+          <button
+            type="button"
+            onClick={() => {
+              fscreen.requestFullscreen(document.body);
+            }}
+          >
+            Fullscreen
+          </button>
+        </h2>
+        <p className="KeyboardInstructions">
           Use the <kbd className="Key">W</kbd>, <kbd className="Key">A</kbd>,{" "}
           <kbd className="Key">S</kbd>, <kbd className="Key">D</kbd> and{" "}
           <kbd className="Key">SPACE</kbd> keys to move.
         </p>
+        <p className="JoystickInstructions">Touch left side to move.</p>
         <p>Bounce in every orange tile to win.</p>
       </section>
     </div>
